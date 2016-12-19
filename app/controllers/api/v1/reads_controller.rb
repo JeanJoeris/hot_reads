142b795d977aa73be2085d6a29c008da7f782d58
Jean Joeris <joeris.peter@gmail.com>
2016-12-19 16:32:20 -0700

    Add functionality for creating a read

--- app/controllers/api/v1/reads_controller.rb
+++ app/controllers/api/v1/reads_controller.rb
@@ -1,4 +1,23 @@
 class Api::V1::ReadsController < ApplicationController
+  before_action :cors_check
   def create
+    @read = Read.new(read_params)
+    if @read.save
+      render json: @read, status: 201
+    else
+      render json: @read.errors.full_messages, status: 500
+    end
+  end
+
+  private
+  def read_params
+    params.permit(:link_id, :link_url, :link_title)
+  end
+
+  def cors_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
--- spec/controllers/read_controller_spec.rb
+++ spec/controllers/read_controller_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe Api::V1::ReadsController do
     response_read = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.status).to eq(201)
-    expect(response_read).to eq(read)
+    expect(response_read[:link_id]).to eq(read[:link_id])
+    expect(response_read[:link_url]).to eq(read[:link_url])
+    expect(response_read[:link_title]).to eq(read[:link_title])
     expect(Read.count).to eq(1)
   end
 end
